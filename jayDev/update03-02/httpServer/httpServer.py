869e1b8effb593ddecdb147f02ab6ecb17339606
zijian-chen96 <47934552+zijian-chen96@users.noreply.github.com>
2020-03-03 05:50:38 -0800

    Add files via upload

---
+++ jayDev/update03-02/httpServer/httpServer.py
@@ -0,0 +1,91 @@
+import sys
+import readline
+import socket
+import time
+import threading
+
+filename0 = "example.txt"
+filename = "user1.txt"
+filename2 = "user2.txt"
+filename3 = "user10.txt"
+filename4 = "user45.txt"
+filename5 = "100User_testWorkLoad"
+filename6 = "workload45.txt"
+
+def recvFromTrans():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('',52000))
+    s.listen(5)
+    cSocket, addr = s.accept()
+
+    while True:
+        data = cSocket.recv(10240).decode()
+
+        # if iList[0] == 'DUMPLOG'and len(iList) == 3:
+        #     f = open(iList[2], "w+")
+        #     while True:
+        #         data = s.recv(10240).decode()
+        #         newData = data[-7:]
+        #         if newData == "the end":
+        #             break
+        #         else:
+        #             f.write(data)
+        #
+        #     f.close()
+        #
+        # elif iList[0] == 'DUMPLOG' and len(iList) == 2:
+        #     f = open(iList[1], "w+")
+        #     while True:
+        #         data = s.recv(10240).decode()
+        #         newData = data[-7:]
+        #         if newData == "the end":
+        #             break
+        #         else:
+        #             f.write(data)
+        #
+        #     f.close()
+        #
+        # elif iList[0] == "DISPLAY_SUMMARY":
+        #     while True:
+        #         data = s.recv(10240).decode()
+        #         #print(len(data))
+        #         newData = data[-7:]
+        #         if newData == "the end":
+        #             #print(newData)
+        #             break
+        #
+        # else:
+        #     data = s.recv(10240).decode()
+        #     #if data:
+        #         #print(data)
+
+
+
+def sendToTrans():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('',50000))
+    alist = [line.rstrip().split(' ')[1] for line in open(filename5)]
+
+    transNum = 0
+    for i in alist:
+        transNum += 1
+        newData = ''
+        newData = ','.join([str(transNum),i])
+        #print('newData send -- ' + newData)
+        s.sendall(newData.encode())
+        data = s.recv(1024).decode()
+        #data = s.recv(1024)
+
+        #iList = i.split(',')
+
+        #print("iLst is -- " + str(iList))
+
+
+    s.send(("finish").encode())
+    s.close()
+
+if __name__=="__main__":
+    th = threading.Thread(target=recvFromTrans)
+    th.start()
+
+    sendToTrans()
